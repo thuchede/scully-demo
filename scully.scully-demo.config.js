513c7adac5813be714a9f32bfd213d00477b081a
Thomas <huchede.thomas@gmail.com>
2020-02-01 20:10:15 +0100

    add blog

--- scully.scully-demo.config.js
+++ scully.scully-demo.config.js
@@ -3,5 +3,11 @@ exports.config = {
   projectName: "scully-demo",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
   },
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
 ];
 
 @NgModule({
